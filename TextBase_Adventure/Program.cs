e9b109c49c6db36680901c556eca14603c0e6dc3
BerendFhost <vwberend@gmail.com>
2024-08-26 10:17:50 +0200

    Add project files.

---
+++ TextBase_Adventure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextBase_Adventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
